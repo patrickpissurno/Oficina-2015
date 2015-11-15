3bd247dce982d3eca3881dc1dab44b489a588373
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2015-11-15 13:22:49 -0200

    Android Win Cutscene is now OK

--- Oficina2015/Assets/Scripts/UI/Menu_Scenes/UI_GameWin.cs
+++ Oficina2015/Assets/Scripts/UI/Menu_Scenes/UI_GameWin.cs
@@ -2,33 +2,32 @@
 using UnityEngine.UI;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class UI_GameWin : MonoBehaviour {
+    public const bool NOTANDROID = true;
+    private RawImage rawImage;
 
-    private Sprite[] AnimSprites;
-    public Image AnimImage;
+    #if NOTANDROID == true
+        public MovieTexture video;
+        private AudioSource audioSource;
 
-    void Start()
-    {
-        /*SoundController._PlayFX("cutscene1");
-        AnimSprites = Resources.LoadAll<Sprite>("Cutscenes/Bus");
-        StartCoroutine(Anim());
-        Application.LoadLevel("MainMenu");*/
-        Invoke("DoPlay", 1.0f);
-    }
-
-    void DoPlay()
-    {
-        Handheld.PlayFullScreenMovie("Cutscene_Win.mp4", Color.black, FullScreenMovieControlMode.Hidden);
-    }
-
-    IEnumerator Anim()
-    {
-        foreach (Sprite frame in AnimSprites)
+        void Start()
         {
-            AnimImage.sprite = frame;
-            yield return new WaitForSeconds(.03f);
+            this.rawImage = this.GetComponent<RawImage>();
+            this.audioSource = this.GetComponent<AudioSource>();
+            this.audioSource.clip = this.video.audioClip;
+            this.video.Play();
+            this.audioSource.Play();
         }
-        Application.LoadLevel("MainMenu");
-    }
-
+    #endif
+    #if NOTANDROID == false
+        void Start()
+        {
+            this.rawImage = this.GetComponent<RawImage>();
+            this.rawImage.color = Color.black;
+            this.rawImage.gameObject.SetActive(false);
+            Handheld.PlayFullScreenMovie("Cutscene_Win.mp4", Color.black, FullScreenMovieControlMode.Hidden, FullScreenMovieScalingMode.Fill);
+            Application.LoadLevel("MainMenu");
+        }
+    #endif
 }
